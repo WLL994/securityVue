a82b5fd95bbce85e478467ba706b4f4f67c29cd0
WLL994 <1017076894@qq.com>
2023-08-03 23:39:43 +0800

    用户管理 分页

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import 'element-plus/dist/index.css'
 import '@/assets/styles/reset.css'
 import '@/assets/styles/border.css'
 import '@/router/permission.js'
+//国际化中文
+import zhCh from 'element-plus/es/locale/lang/zh-cn';
 
 import SvgIcon from '@/icons'
 
@@ -14,6 +16,9 @@ import SvgIcon from '@/icons'
 const app=createApp(App);
 SvgIcon(app);
 
+app.use(ElementPlus,{
+    locale:zhCh
+})
 
 app.use(store)
 app.use(router)
--- src/views/sys/user/index.vue
+++ src/views/sys/user/index.vue
@@ -1,11 +1,76 @@
 <template>
-用户管理
+  <div class="app-container">
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="username" label="用户名" width="180" />
+    </el-table>
+  </div>
+  <el-pagination
+      v-model:current-page="queryForm.pageNum"
+      v-model:page-size="queryForm.pageSize"
+      :page-sizes="[5, 10, 15, 20]"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+  />
 </template>
 
 <script setup>
+import {ref} from "vue";
+import request,{getServerUrl} from "@/utils/request";
 
+const tableData=ref([])
+
+const total=ref(0)
+
+const queryForm=ref({
+  query:'',
+  pageNum:1,
+  pageSize:10
+})
+
+const  initUserList = async ()=>{
+  const res=await request.post("sys/user/list",queryForm.value);
+  tableData.value=res.data.userList;
+  total.value=res.data.total;
+}
+
+initUserList();
+
+const handleSizeChange=(pageSize)=>{
+  queryForm.value.pageNum=1;
+  queryForm.value.pageSize=pageSize;
+  initUserList();
+}
+
+const handleCurrentChange=(pageNum)=>{
+  queryForm.value.pageNum=pageNum;
+  initUserList();
+}
 </script>
 
 <style scoped lang="scss">
+.header{
+  padding-bottom: 16px;
+  box-sizing: border-box;
+}
 
+.el-pagination{
+  float: right;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+::v-deep th.el-table__cell{
+  word-break: break-word;
+  background-color: #f8f8f9 !important;
+  color: #515a6e;
+  height: 40px;
+  font-size: 13px;
+
+}
+
+.el-tag--small {
+  margin-left: 5px;
+}
 </style>
